632c2b1e1b6347c481ec05535cea43200d76bd98
nandu890 <93024230+likhitha1186@users.noreply.github.com>
2026-07-09 22:23:42 +0530

    added new files

--- concepts/11encapsulation.js
+++ concepts/11encapsulation.js
@@ -54,8 +54,74 @@ class ApiClient {
 }
 
 const client = new ApiClient("xyz123");
-console.log(client.getAuthHeader()); // works
-console.log(client.#authToken);      // Error: private field
+console.log(client.getAuthHeader()); // Bearer xyz123
+//console.log(client.#authToken);      // Error: private field
 
-/*In your REST/SOAP API testing utility classes (Postman/ReadyAPI scripting or custom JS API clients), the auth token or session cookie is kept private and only exposed through controlled methods
+
+function outer(){
+    let message="Hello";
+
+    function inner(){
+        console.log(message); //Hello
+    }
+    return inner;
+}
+const greet=outer();
+greet();
+console.log(greet()); //undefined
+
+function outer1(){
+    let message="Hello";
+
+    function inner1(){
+        console.log(message); //Hello
+        return message;  //Hello
+    }
+    return inner1;
+}
+const greet1=outer1();
+greet1();
+console.log(greet1());  //Hello
+
+
+function createLoginTracker(userName) {
+    let loginAttempts = 0; // private variable
+
+    function trackLogin() {
+        loginAttempts++;
+        console.log(`${userName} login attempt: ${loginAttempts}`);
+    }
+
+    return trackLogin;
+}
+
+const userLogin = createLoginTracker("Likhitha");
+
+userLogin(); //Likhitha login attempt: 1
+userLogin();  //Likhitha login attempt: 2
+userLogin();  //Likhitha login attempt: 3
+
+/*
+This code demonstrates encapsulation in JavaScript. ApiClient is a class, and #authToken is a private field that stores the authentication token.
+The constructor initializes this private field when an object is created.
+ The getAuthHeader() method is a public method that accesses the private token and returns it in the format Bearer <token>.
+  Since #authToken is private, it cannot be accessed directly from outside the class, which protects sensitive data and ensures controlled access.
+This is a good example of encapsulation because the data is hidden and exposed only through public methods.
+
+A closure is an inner function that remembers and can access the variables of its outer function, even after the outer function has finished executing.
+It allows us to reuse variables without passing them as arguments every time. In Playwright, closures are commonly used in helper functions and callbacks, making the code cleaner and easier to maintain.
+
+"A closure means an inner function remembers its parent function's variables."
+function outer(){
+ let message="Hello";
+
+ function inner(){
+ console.log(message);
+ }
+ return inner;
+ }
+ const greet=outer();
+ greet();
+
+In your REST/SOAP API testing utility classes (Postman/ReadyAPI scripting or custom JS API clients), the auth token or session cookie is kept private and only exposed through controlled methods
 */
